4765de85e6a7c960851b2b05f44e1bc8ecbde129
[LG유플러스] 박소현 <shpark1125@lguplus.co.kr>
2023-08-31 17:01:27 +0900

    init

---
+++ layouts/error.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h3>Error</h3>
+    <NuxtLink to="/">go home</NuxtLink>
+  </div>
+</template>
---
+++ store/sample/index.js
@@ -0,0 +1,11 @@
+export const state = () => ({
+  validateResponse: null,
+})
+
+export const mutations = {
+  setValidateResponse(state, payload) {
+    state.validateResponse = payload
+  }
+}
+
+export const actions = {}
