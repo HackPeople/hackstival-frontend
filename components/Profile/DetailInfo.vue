2a7d8e944281a65a021f5df93f534bb14fa87153
[LG유플러스] 윤재원 <yjw68@lguplus.co.kr>
2023-09-01 03:31:53 +0900

    승인 했다~

---
+++ components/Profile/DetailInfo.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="profile-inner-wrap">
+    <img class="image" :src="helperInfo.profileImage"/>
+    <div class="info-card">
+      <p class="name">{{helperInfo.userName}}</p>
+      <p class="id">{{helperInfo.userId}}</p>
+      <div class="icon-group">
+        <button class="circle-button purple">
+          <span class="material-icons-outlined button-icon">
+            list
+          </span>
+        </button>
+        <button class="circle-button blue">
+          <span class="material-icons-outlined button-icon">
+            chat
+          </span>
+        </button>
+        <button class="circle-button green">
+          <span class="material-icons-outlined button-icon">
+            call
+          </span>
+        </button>
+      </div>
+      <div class="score">
+        <img class="rice" src="@/assets/image/free-icon-rice-129356.png"/>
+        <img class="rice" src="@/assets/image/free-icon-rice-129356.png"/>
+        <img class="rice" src="@/assets/image/free-icon-rice-129356.png"/>
+        <img class="rice" src="@/assets/image/free-icon-rice-129356.png"/>
+        <img class="rice" src="@/assets/image/free-icon-rice-129356.png"/>
+      </div>
+    </div>
+    <div class="button-wrap">
+      <button class="select-button select-button-1" @click="accept">
+        수락하기
+      </button>
+      <button class="select-button select-button-2">
+        거절하기
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    helperInfo: {
+      type: Object,
+      default: () => {}
+    },
+    helperId: {
+      type: Number,
+      default: 0
+    },
+    helpId: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    async accept() {
+      try {
+        await this.$axios.$put(`/api/help/confirm?helpId=${this.helpId}&helperId=${this.helperId}`)
+        this.$router.push('/past/list')
+      } catch(e){
+        console.log('에러 발생', e)
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.profile-inner-wrap {
+  display: flex;
+  height: 100px;
+  margin-top: 20px;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.image {
+  height: 100%;
+  border-radius: 50%;
+  z-index: 10;
+}
+
+.info-card{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 80px;
+  margin-top: -50px;
+  min-height: 300px;
+  width: 80%;
+  border: 1px solid #e5e5e5;
+  border-radius: 12px;
+  box-shadow: -1px 3px 15px 0 rgba(0,0,0,.06);
+}
+
+.name{
+  font-weight: bold;
+  font-size: 32px;
+  padding-bottom: 10px;
+}
+
+.id{
+  color: gray;
+  padding-bottom: 40px;
+}
+
+.phone-number{
+  display: flex;
+  align-items: center;
+}
+
+.icon-group{
+  display: flex;
+  justify-content: center;
+  padding-bottom: 20px;
+}
+
+.circle-button{
+  height: 60px;
+  width: 60px;
+  margin: 10px;
+  border: 0;
+  background-color: transparent;
+  border-radius: 50%;
+  color: white;
+}
+
+.button-icon{
+  font-size: 32px;
+  font-weight: bolder;
+}
+
+.purple{
+  background-color: #593FD4;
+}
+
+.purple:hover{
+  background-color: #391CCA;
+  cursor: pointer;
+}
+
+.blue{
+  background-color: #1275D3;
+}
+
+.blue:hover{
+  background-color: #075EB2;
+  cursor: pointer;
+}
+
+.green{
+  background-color: #12C764;
+}
+
+.green:hover{
+  background-color: #00AD50;
+  cursor: pointer;
+}
+
+.rice{
+  margin: 5px;
+  height: 40px;
+}
+
+.button-wrap{
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+}
+
+.select-button{
+  margin: 10px;
+  height: 60px;
+  width: 200px;
+  border: 0;
+  border-radius: 20px;
+  font-size: 20px;
+}
+
+.select-button-1{
+  color: gray;
+  background-color: #FFE47A;
+}
+
+.select-button-1:hover{
+  background-color: #E2BC50;
+  cursor: pointer;
+}
+
+.select-button-2{
+  color: gray;
+  background-color: white;
+  border: solid 3px #E2BC50;
+}
+
+.select-button-2:hover{
+  color: gray;
+  background-color: transparent;
+  border: solid 3px #E2BC50;
+  cursor: pointer;
+}
+
+</style>
